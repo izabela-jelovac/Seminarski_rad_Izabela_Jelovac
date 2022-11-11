5073e23f281ed6bb01bb8f7aafd5a3a1c97889b6
Izabela <104584812+izabela-jelovac@users.noreply.github.com>
2022-11-11 17:14:35 +0100

    log written text into console on click of the button send

--- web-chat-app/src/MessageInput.js
+++ web-chat-app/src/MessageInput.js
@@ -1,4 +1,29 @@
+import { useState } from "react";
+
 function MessageInput() {
-  return <div>TODO: message input</div>;
+  const placeholder = "Enter your message and press ENTER";
+  const [messageInputValue, setMessageInputValue] = useState("");
+
+  const handleChange = (event) => {
+    setMessageInputValue(event.target.value);
+  };
+
+  const handleClick = (event) => {
+    console.log(messageInputValue);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        onChange={handleChange}
+        value={messageInputValue}
+        placeholder={placeholder}
+      />
+      <button type="button" onClick={handleClick}>
+        Send
+      </button>
+    </div>
+  );
 }
 export default MessageInput;
